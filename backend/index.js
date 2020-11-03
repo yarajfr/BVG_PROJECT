0113f59ffe59dce68dbc9350a6f0fc2f4df1b881
yarajfr <s0569672@htw-berlin.de>
2020-11-04 00:42:05 +0100

    yara eigene datenbank daten eingefügt und elifs gelöscht

--- backend/index.js
+++ backend/index.js
@@ -30,8 +30,8 @@ let mysql = require('mysql');
 
 let connection = mysql.createConnection({
     host: 'localhost',
-    user: 'elif',
-    password: 'elif',
+    user: 'yara',
+    password: 'gWgTvojnDhS1ugHB',
     database: 'BVG_Daten'
 
 });
